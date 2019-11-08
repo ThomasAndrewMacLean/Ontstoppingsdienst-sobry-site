9974b7bb64f30f92e295e6e4dd99fa8e70f539d8
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-11-08 15:41:54 +0100

    add icons

---
+++ src/components/NavItem.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let link;
+  export let icon;
+  export let name;
+</script>
+
+<style>
+  /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here -->
+
+<a href={link}>
+  <img src={icon} alt={name} />
+  <h4>{name}</h4>
+</a>
