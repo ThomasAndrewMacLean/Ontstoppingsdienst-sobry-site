611ed64914b935012d0abec12780d0bc53ae4af9
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-11-11 01:33:18 +0100

    topnav

--- src/components/TopNav.svelte
+++ src/components/TopNav.svelte
@@ -1,9 +1,42 @@
 <script>
-  // your script goes here
+    // your script goes here
 </script>
 
 <style>
-  /* your styles go here */
+    nav {
+        background: var(--dark-background);
+        margin: 0;
+        font-size: 14px;
+        padding: 5px;
+        color: var(--white-text);
+    }
+    ul {
+        margin: 0;
+        display: flex;
+        justify-content: flex-end;
+    }
+    li {
+        margin-right: 1rem;
+    }
 </style>
 
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/">Over mij</a>
+        </li>
+        <li>
+            <a href="/">Prijzen</a>
+        </li>
+        <li>
+            <a href="/">Tips</a>
+        </li>
+        <li>
+            <a href="/">Contact</a>
+        </li>
+    </ul>
+</nav>
 <!-- markup (zero or more items) goes here -->
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,24 +1,26 @@
 <script>
-  import Nav from "../components/Nav.svelte";
-  import BottomNav from "../components/BottomNav.svelte";
-  import labels from "../labels.js";
-  export let segment;
+    import Nav from '../components/Nav.svelte';
+    import TopNav from '../components/TopNav.svelte';
+    import BottomNav from '../components/BottomNav.svelte';
+    import labels from '../labels.js';
+    export let segment;
 </script>
 
 <style>
-  main {
-    position: relative;
-    max-width: 56em;
-    background-color: var(--white-background);
-    padding: 0 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
+    main {
+        position: relative;
+        max-width: 56em;
+        background-color: var(--white-background);
+        padding: 0 2em;
+        margin: 0 auto;
+        box-sizing: border-box;
+    }
 </style>
 
+<TopNav />
 <Nav {segment} {labels} />
 
 <main>
-  <slot />
+    <slot />
 </main>
 <BottomNav {labels} {segment} />
