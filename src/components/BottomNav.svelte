d99a31a83b5cda6420d789e3c1ac9b1a212316f3
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-11-11 01:50:36 +0100

    hero🚤

--- src/components/BottomNav.svelte
+++ src/components/BottomNav.svelte
@@ -1,51 +1,50 @@
 <script>
-  export let segment;
-  export let labels;
+    export let segment;
+    export let labels;
 </script>
 
 <style>
-  #bottom-nav {
-    background: var(--dark-background);
-    color: var(--white-text);
-    display: flex;
-    justify-content: space-between;
-    padding: 2rem 0;
-  }
-  .contact {
-    text-align: right;
-    margin-right: 40px;
-  }
+    #bottom-nav {
+        background: var(--dark-background);
+        color: var(--white-text);
+        display: flex;
+        font-size: 14px;
+        justify-content: space-between;
+        padding: 2rem 0;
+    }
+    .contact {
+        text-align: right;
+        margin-right: 40px;
+    }
 </style>
 
 <div id="bottom-nav">
-  <nav>
-    <ul>
-      <li>
-        <a rel="prefetch" href="ontstoppen">{labels.ontstoppen}</a>
-      </li>
-      <li>
-        <a rel="prefetch" href="cameradetectie">{labels.cameradetectie}</a>
-      </li>
-      <li>
-        <a rel="prefetch" href="geurdetectie">{labels.geurdetectie}</a>
-      </li>
-      <li>
-        <a rel="prefetch" href="adviesaankoopwoning">
-          {labels.adviesaankoopwoning}
-        </a>
-      </li>
-      <li>
-        <a rel="prefetch" href="ontkalken">{labels.ontkalken}</a>
-      </li>
-      <li>
-        <a rel="prefetch" href="herstellingen">{labels.herstellingen}</a>
-      </li>
-    </ul>
-  </nav>
-  <div class="contact">
-    <ul>
-      <li>{labels.telefoonnummerlabel}: {labels.telefoonnummer}</li>
-      <li>{labels.emaillabel}: {labels.email}</li>
-    </ul>
-  </div>
+    <nav>
+        <ul>
+            <li>
+                <a rel="prefetch" href="ontstoppen">{labels.ontstoppen}</a>
+            </li>
+            <li>
+                <a rel="prefetch" href="cameradetectie">{labels.cameradetectie}</a>
+            </li>
+            <li>
+                <a rel="prefetch" href="geurdetectie">{labels.geurdetectie}</a>
+            </li>
+            <li>
+                <a rel="prefetch" href="adviesaankoopwoning">{labels.adviesaankoopwoning}</a>
+            </li>
+            <li>
+                <a rel="prefetch" href="ontkalken">{labels.ontkalken}</a>
+            </li>
+            <li>
+                <a rel="prefetch" href="herstellingen">{labels.herstellingen}</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="contact">
+        <ul>
+            <li>{labels.telefoonnummerlabel}: {labels.telefoonnummer}</li>
+            <li>{labels.emaillabel}: {labels.email}</li>
+        </ul>
+    </div>
 </div>
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,107 +1,105 @@
 <script>
-  import { onMount } from "svelte";
-  let afspraakHero;
-  let afspraakOnder;
-  export let labels;
+    import { onMount } from 'svelte';
+    let afspraakHero;
+    let afspraakOnder;
+    export let labels;
 
-  onMount(() => {
-    const obsOptions = { rootMargin: "-200px 0px 0px 0px" };
+    onMount(() => {
+        const obsOptions = { rootMargin: '-200px 0px 0px 0px' };
 
-    const afspraakObserver = new IntersectionObserver(function(
-      entries,
-      afspraakObserver
-    ) {
-      entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-          afspraakOnder.style.bottom = "50px";
-        } else {
-          afspraakOnder.style.bottom = "-100px";
-        }
-      });
-    },
-    obsOptions);
+        const afspraakObserver = new IntersectionObserver(function(entries, afspraakObserver) {
+            entries.forEach(entry => {
+                if (!entry.isIntersecting) {
+                    afspraakOnder.style.bottom = '50px';
+                } else {
+                    afspraakOnder.style.bottom = '-100px';
+                }
+            });
+        }, obsOptions);
 
-    afspraakObserver.observe(afspraakHero);
-  });
+        afspraakObserver.observe(afspraakHero);
+    });
 </script>
 
 <style>
-  #hero {
-    background: var(--light-background);
-    width: 100vw;
-    /* position: absolute; */
-    margin-left: calc(-50vw + 26em);
-
-    left: 0;
-    right: 0;
-  }
-
-  @media (max-width: 56em) {
     #hero {
-      margin-left: -32px;
-    }
-  }
-  .center {
-    position: relative;
-    /* max-width: 56em; */
-    padding: 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
+        background: var(--light-background);
+        width: 100vw;
+        /* position: absolute; */
+        margin-left: calc(-50vw + 26em);
 
-    display: flex;
-    justify-content: space-around;
-  }
-  .afspraak {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background: var(--dark-background);
-    padding: 1rem 0.5rem;
-    color: var(--white-text);
-    cursor: pointer;
-    /* border: 2px solid var(--accent-primary); */
-    border-radius: var(--border-radius);
-  }
-  img {
-    height: 100%;
-  }
-  .logo {
-    padding: 2rem;
-  }
-  .left {
-    display: flex;
-    flex-direction: column;
-    /* width: 40vw; */
-  }
-  /* .right {
+        left: 0;
+        right: 0;
+    }
+
+    @media (max-width: 56em) {
+        #hero {
+            margin-left: -32px;
+        }
+    }
+    .center {
+        position: relative;
+        /* max-width: 56em; */
+        /* padding: 2em; */
+        margin: 0 auto;
+        box-sizing: border-box;
+
+        display: flex;
+        justify-content: space-around;
+    }
+    .afspraak {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background: var(--dark-background);
+        padding: 1rem 0.5rem;
+        margin: 1rem;
+        color: var(--white-text);
+        cursor: pointer;
+        /* border: 2px solid var(--accent-primary); */
+        border-radius: var(--border-radius);
+    }
+    img {
+        height: 100%;
+    }
+    .logo {
+        padding: 1rem;
+    }
+    .left {
+        display: flex;
+        flex-direction: column;
+        /* width: 40vw; */
+        /* padding: 2rem; */
+    }
+    /* .right {
      width: 60vw; 
   } */
 
-  .afspraakOnder {
-    background: var(--accent-secondary);
-    position: fixed;
-    bottom: -100px;
-    right: 50px;
-    transition: bottom 200ms ease-in;
-  }
+    .afspraakOnder {
+        background: var(--accent-secondary);
+        position: fixed;
+        bottom: -100px;
+        right: 50px;
+        transition: bottom 200ms ease-in;
+    }
 </style>
 
 <div id="hero">
-  <div class="center">
-    <div class="left">
-      <img class="logo" src="logo.png" alt="logo Sobry ontstoppingsdienst" />
-      <div class="afspraak" bind:this={afspraakHero}>
-        <span>{labels.afspraak}</span>
-        <span>{labels.telefoonnummer}</span>
-      </div>
-      <div class="afspraak afspraakOnder" bind:this={afspraakOnder}>
-        <span>{labels.afspraak}</span>
-        <span>{labels.telefoonnummer}</span>
-      </div>
+    <div class="center">
+        <div class="left">
+            <img class="logo" src="logo.png" alt="logo Sobry ontstoppingsdienst" />
+            <div class="afspraak" bind:this={afspraakHero}>
+                <span>{labels.afspraak}</span>
+                <span>{labels.telefoonnummer}</span>
+            </div>
+            <div class="afspraak afspraakOnder" bind:this={afspraakOnder}>
+                <span>{labels.afspraak}</span>
+                <span>{labels.telefoonnummer}</span>
+            </div>
 
+        </div>
+        <div class="right">
+            <img src="house.png" alt="image of a houseand a plumber" />
+        </div>
     </div>
-    <div class="right">
-      <img src="house.png" alt="image of a houseand a plumber" />
-    </div>
-  </div>
 </div>
