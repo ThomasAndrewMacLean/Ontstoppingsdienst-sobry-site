7462af78c703767d7df404cb1ed7141a549207ab
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-11-23 12:55:43 +0100

    overlay image logo

--- src/components/DienstPagina.svelte
+++ src/components/DienstPagina.svelte
@@ -7,22 +7,33 @@
 </script>
 
 <style>
-    img {
-        height: 200px;
+    .full-width-picture {
+        height: 450px;
         margin: 0;
         object-fit: cover;
         object-position: bottom;
         width: 102vw;
-        margin-left: calc(-50vw + 26em);
+        margin-left: calc(-50vw + 485px);
     }
-    @media (max-width: 56em) {
-        img {
-            margin-left: -32px;
+    .image-wrap {
+        position: relative;
+    }
+    .logo {
+        position: absolute;
+        top: 5%;
+        height: 80%;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+    @media (max-width: 1000px) {
+        .full-width-picture {
+            margin-left: -3%;
+            width: 106%;
         }
     }
 
     .text-blok {
-        padding: 2rem;
+        padding: 2rem 0;
         display: flex;
         flex-direction: column;
     }
@@ -32,7 +43,10 @@
     }
 </style>
 
-<img alt={title} src={image} />
+<div class="image-wrap">
+    <img class="full-width-picture" alt={title} src={image} />
+    <img class="logo" src="logo_transparant.png" alt="sobry logo overlay" />
+</div>
 <div class="text-blok">
     <h2>{title}</h2>
     <p>{text}</p>
