057b0a27abc1d1c5187c2469ed144a0e5542f7a8
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-11-24 12:54:22 +0100

    remove dodgy labels

--- src/labels.js
+++ src/labels.js
@@ -3,7 +3,7 @@ const labels = {
     afspraak: 'Maak een afspraak',
     ontstoppen: 'Ontstoppen',
     ontstoppentext:
-        'Ontstoppingen & Verstoppingen geven tal van ongemakken. Je dagelijks comfort in je huis of in je bedrijfspand wordt ernstig verstoord. Denk aan een verstopte WC of water dat niet wegstroomt in je bad, lavabo en/of gootsteen, …  Dit kan tevens hinderlijke afvoergeuren.',
+        'Ontstoppingen & Verstoppingen geven tal van ongemakken. Je dagelijks comfort in je huis of in je bedrijfspand wordt ernstig verstoord. Denk aan een verstopte WC of water dat niet wegstroomt in je bad, lavabo en/of gootsteen, … Dit kan tevens hinderlijke afvoergeuren.',
     cameradetectie: 'Cameradetectie',
     cameradetectietext:
         'Uw rioolprobleem, putten en leidingen detecteren Een riolering kan om verschillende redenen aan herstelling toe zijn: verzakkingen, roest, ingegroeide boomwortels, of gewoon slijtage. Vermoed u dat er een probleem is in uw riool, maar kunt u de oorzaak niet vinden? Aan de hand van een cameradetectie kunt u het probleem detecteren.',
@@ -15,7 +15,7 @@ const labels = {
         'Bij aankoop van een bestaande/nieuwbouw woning is het rioolnetwerk niet zichtbaar. Doormiddel van camera onderzoek kunnen we het netwerk in kaart brengen, kijken in welke staat uw riolering is en eventueel zwakke punten te melden en aan te pakken. Mooi voorbeeld zijn verdoken putten, verzakkingen,etc… Dit kan belangrijk zijn bij renovatiewerken, zo voorkomt u  onaangename verassingen.',
     ontkalken: 'Ontkalken',
     ontkalkentext:
-        'Is er kalk in uw leiding of zijn er wortels in uw rioolnetwerk gekomen,dan kunnen wij zonder breken deze verwijderen doormiddel van onze geadvanceerd materiaal. Bijvoorbeeld aan de hand van een camera inspectie. Voorkom problemen door twee maal per jaar uw leidingen te laten controleren',
+        'Is er kalk in uw leiding of zijn er wortels in uw rioolnetwerk gekomen,dan kunnen wij zonder breken deze verwijderen doormiddel van onze geadvanceerd materiaal. Bijvoorbeeld aan de hand van een camera inspectie. Voorkom problemen door twee maal per jaar uw leidingen te laten controleren',
     herstellingen: 'Herstellingen',
     herstellingentext:
         'Een lek in uw riolering, afvoer of put? Je merkt het meestal aan de typische rioolgeur en een trager werkende afvoer. Dit kan op twee manieren opgelost worden: je kan de leiding in kwestie laten vervangen of je kan deze laten herstellen. Doorgaans wordt er gekozen voor een reparatie, bij een rioolrenovatie en/ of leidingwerk kunt u het lek in de riolering immers dichten zonder opgraafwerk. ',
