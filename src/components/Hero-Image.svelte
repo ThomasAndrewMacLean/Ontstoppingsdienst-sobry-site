b48aff4a5e64c12623af94426e2b4f1214bc40b3
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-12-03 12:16:17 +0100

    add parallax effect 👯‍♀️

--- src/components/Hero-Image.svelte
+++ src/components/Hero-Image.svelte
@@ -11,10 +11,24 @@
         width: 100vw;
         margin-left: calc(-50vw + 500px);
         filter: grayscale(0.9);
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
     }
     .image-wrap {
         position: relative;
     }
+
+    /* .image-wrap::before {
+        height: 450px;
+        margin: 0;
+        object-fit: cover;
+        object-position: bottom;
+        width: 100vw;
+        margin-left: calc(-50vw + 500px);
+        filter: grayscale(0.9);
+    } */
 
     @media screen and (-ms-high-contrast: active), (-ms-high-contrast: none) {
         .full-width-picture {
@@ -40,10 +54,15 @@
             width: 100%;
         }
     }
+
+    @media (max-width: 900px) {
+        .full-width-picture {
+            background-attachment: scroll;
+        }
+    }
 </style>
 
 <div class="image-wrap">
-    <img class="full-width-picture" alt="image-hero" src={image} />
-
+    <div class="full-width-picture" style="background-image:url({image})" />
     <img class="logo" src="logo_transparant.png" alt="sobry logo overlay" />
 </div>
